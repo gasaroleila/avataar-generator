ff9d47a747670b97aaf077e1a4f6d5a4a41cfb33
igaimerca <igaimerca@gmail.com>
2022-03-07 21:01:03 +0200

    fix: generate hash for the name

--- src/utils/colorCombination.tsx
+++ src/utils/colorCombination.tsx
@@ -1,23 +1,23 @@
-import * as crypto from 'crypto';
+import { SHA256 } from 'crypto-ts';
+
 import * as colors from './colors';
 
 // Get a valid seed
-const getValidSeed = (name:any) => {
-    let hash = crypto.createHash('md5').update(name).digest('hex');
-    let seed = String(hash.replace(/\D/g,''))
+const getValidSeed = (name: any) => {
+    const hash = SHA256(name).toString();
+    let seed = String(hash.replace(/\D/g, ''))
     return seed;
 }
 
 // Getting index level 1
-const getColorObject = (dig_one:any) => {
-    let index1:number;
+const getColorObject = (dig_one: any) => {
+    let index1: number;
     index1 = dig_one == 9 ? dig_one : dig_one + 1;
-    console.log('index'+index1)
     return colors.colors[index1];
 }
 
 // Getting index level 2
-const getExactColor = (dig_one:any, dig_two:any) => {
+const getExactColor = (dig_one: any, dig_two: any) => {
     let index2;
     let binaryValue = dig_two.toString(2) == 0 ? 0 : dig_two.toString(2);
     let colorObject = getColorObject(dig_one);
@@ -29,8 +29,10 @@ const getExactColor = (dig_one:any, dig_two:any) => {
         return colorObject[index2];
     }
 }
-export const returnColors = (name:string) => {
+export const returnColors = (name: string) => {
     let seed = getValidSeed(name);
+
+
     let colorObj;
     if (seed.length <= 1) {
         colorObj = { bg: "#FFFFFF", front: "#0D4D8C" };
@@ -39,9 +41,9 @@ export const returnColors = (name:string) => {
         let dig_two = Number(seed[1]);
 
         let color = getExactColor(dig_one, dig_two);
-        if(dig_one >= dig_two) {
+        if (dig_one >= dig_two) {
             colorObj = { bg: "#FFFFFF", front: color };
-        }else {
+        } else {
             colorObj = { bg: color, front: "#FFFFFF" }
         }
     }
