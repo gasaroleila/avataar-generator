99e8be8d8c24b2106f7950c25da0b232dbf3aecd
igaimerca <igaimerca@gmail.com>
2022-03-07 21:14:53 +0200

    fix: hash names

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -6,8 +6,20 @@ function Avatar() {
   const [name, setName] = useState('Leila')
     let frontColor;
     let backColor;
-    document.documentElement.style.setProperty("--main-front-color", "#FFFFFF");
-    document.documentElement.style.setProperty("--main-back-color", "#000000");
+    console.log(colorCombinator.returnColors('dddk'))
+    frontColor = Math.floor(Math.random() * 16777215).toString(16);
+    backColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    document.documentElement.style.setProperty("--main-front-color", "#" + frontColor);
+    while ("#" + frontColor == "#" + backColor && frontColor < "B0BEC5") {
+        backColor = Math.floor(Math.random() * 16777215).toString(16);
+        if ("#" + backColor == "#e1e3ee") {
+            frontColor = Math.floor(Math.random() * 16777215).toString(16);
+        } else if ("#" + frontColor == "#e1e3ee") {
+            backColor = Math.floor(Math.random() * 16777215).toString(16);
+        }
+    }
+    document.documentElement.style.setProperty("--main-back-color", "#FFF");
     return (
         <div className="outer">
             <div className="inner"></div>
